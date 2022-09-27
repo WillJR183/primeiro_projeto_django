fc5fc4b33bebc593370e58deef073344f3ff4558
Will JR <junior.stuart34@gmail.com>
2022-09-27 17:04:46 -0400

    templates criados, uso do render e namespace

--- receitas/views.py
+++ receitas/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+# A função render do Django é responsável por ler e renderizar um arquivo web
+# Sem ela, teríamos que escrever código web dentro das funções das views
+
+# Outro ponto muito importante do Django é o uso de namespace
+# namespace em resumo é uma metodologia empregada na hora de nomear
+# rotas (URLs) e templates, sempre utilizando um diretório que se 
+# refere ao app, no recurso de template ex: receitas/templates/receitas/index.html
+
 def pag_inicial(request):
-    return HttpResponse('<strong> Página Inicial <strong>')
+    return render(request, 'receitas/pag_inicial.html')
 
 def pag_contato(request):
-    return HttpResponse('<strong> Página de Informações de Contato <strong>')
+    return render(request, 'receitas/pag_contatos.html')
