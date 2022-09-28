8b8379680e962b66539449eebf5073b7bf805690
Will JR <junior.stuart34@gmail.com>
2022-09-28 11:23:44 -0400

    separando trecho fixo do HTML e incluindo(include) em pag_inicial.html

--- receitas/views.py
+++ receitas/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def pag_inicial(request):
-    return render(request, 'receitas/pag_inicial.html', context = {
+    return render(request, 'receitas/pages/pag_inicial.html', context = {
         'name':'Willian JR.'
     })
