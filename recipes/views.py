9e4c52057c546480354a48844d9839c854fed0d8
Will JR <junior.stuart34@gmail.com>
2022-09-29 07:56:56 -0400

    cria view de detalhes da receita e adiciona nova rota

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context = {
         'name':'Willian JR.'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context = {
+        'name':'Willian JR.'
+    })
