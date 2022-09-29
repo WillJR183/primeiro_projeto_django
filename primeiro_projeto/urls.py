1745dc05336f0207ed53f07122889f608db01649
Will JR <junior.stuart34@gmail.com>
2022-09-29 16:07:27 -0400

    aplica model Recipe dinamicamente na view home

--- primeiro_projeto/urls.py
+++ primeiro_projeto/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Rotas
 
@@ -7,3 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))
 ]
+
+# Conf de URL dos arquivos estáticos
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
