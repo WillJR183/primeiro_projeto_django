bdcb7ade371486d4b27b597b72c8088c66865a51
Will JR <junior.stuart34@gmail.com>
2022-09-29 11:37:40 -0400

    gera url com id dos cards dinamicamente e usa app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 # Rotas
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('', views.home, name="home"), 
+    path('recipes/<int:id>/', views.recipe, name="recipe")
 ]
